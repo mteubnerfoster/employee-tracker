4ec8902a85c8b3bc8a94976d6ca50d91d836dd60
Megan Teubner-Foster <mteubnerfoster@gmail.com>
2021-12-07 20:37:57 -0800

    Added asciiart-logo to dependencies for logo
    Added asciiart-logo to dependencies for logo

--- db/connection.js
+++ db/connection.js
@@ -4,7 +4,7 @@ const mysql = require('mysql2');
 const util = require('util');
 
 const connection = mysql.createConnection({
-    host: 'localhost',
+    host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: 'employee_tracker_DB',
--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 require("dotenv").config();
+const logo = require('asciiart-logo');
+const config = require('./package.json');
+console.log(logo(config).render());
 const inquirer = require('inquirer');
 require('console.table');
 const db = require('./db/methods.js');
